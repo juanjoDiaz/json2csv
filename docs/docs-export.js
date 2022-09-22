5e828fc06918576c2a672f136df193efab3e42b2
Juanjo Diaz <juanjo.diazmo@gmail.com>
2022-09-22 23:32:50 +0200

    chore: remove unused file docs-export.js

--- docs/docs-export.js
+++
@@ -1,5 +0,0 @@
-import StreamParser from '../packages/plainjs/src/StreamParser.js';
-window.Json2csvStreamParser = StreamParser;
-
-import { unwind, flatten } from '../packages/transforms/src/index';
-window.Json2csvTransforms = { unwind, flatten };
