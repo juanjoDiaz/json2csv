4bde41e86483e171a30d0cfd743a5e841ffa8ddc
Juanjo Díaz <juanjo.diazmo@gmail.com>
2026-01-20 00:14:18 +0100

    chore: update typescript to latest

--- packages/formatters/src/Formatter.ts
+++ packages/formatters/src/Formatter.ts
@@ -1,5 +1,3 @@
 type Formatter<T> = (a: T) => string;
 
-// biome-ignore lint/suspicious/noTsIgnore: Required for verbatimModuleSyntax builds - see https://github.com/microsoft/TypeScript/issues/41409
-// @ts-ignore
-export default Formatter;
+export type { Formatter as default };
--- packages/transforms/src/Transform.ts
+++ packages/transforms/src/Transform.ts
@@ -1,5 +1,3 @@
 type Transform<I, O> = (input: I) => O[];
 
-// biome-ignore lint/suspicious/noTsIgnore: Required for verbatimModuleSyntax builds - see https://github.com/microsoft/TypeScript/issues/41409
-// @ts-ignore
-export default Transform;
+export type { Transform as default };
